6740721e2544f17cbf87a50d9d1a83dcf565b973
FrozenFisher <kuw9549@dingtalk.com>
2026-03-10 16:55:15 +0800

    update: intake button

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -184,7 +184,14 @@ public class RobotContainer {
                     drive)
                 .ignoringDisable(true));
 
-    driverController.rightBumper().onTrue(Commands.runOnce(superStructure::toggleIntakeMode));
+    // driverController
+    //     .rightBumper()
+    //     .onTrue(superStructure.runOnce(() -> superStructure.setIntakeMode(SuperStructure.IntakeMode.INTAKE)))
+    //     .onFalse(superStructure.runOnce(() -> superStructure.setIntakeMode(SuperStructure.IntakeMode.SHAKE)));
+    
+    // driverController
+    //     .leftBumper()
+    //     .onTrue(superStructure.runOnce(() -> superStructure.setIntakeMode(SuperStructure.IntakeMode.OFF)));
 
     driverController
         .leftBumper()
@@ -218,6 +225,8 @@ public class RobotContainer {
                     () -> -driverController.getLeftX(),
                     () -> -driverController.getRightX(),
                     2.)));
+    intake.setDefaultCommand(new IntakeDefaultCommand());
+
     // intake.setDefaultCommand(
     //     Commands.defer(() -> superStructure.getIntakeCommand(), Set.of(intake, stretcher)));
 
@@ -246,8 +255,12 @@ public class RobotContainer {
 
     driverController
         .rightBumper()
-        .onTrue(superStructure.runOnce(() -> superStructure.toggleIntakeMode()));
-    intake.setDefaultCommand(new IntakeDefaultCommand());
+        .onTrue(superStructure.runOnce(() -> superStructure.setIntakeMode(SuperStructure.IntakeMode.INTAKE)))
+        .onFalse(superStructure.runOnce(() -> superStructure.setIntakeMode(SuperStructure.IntakeMode.SHAKE)));
+    
+    driverController
+        .leftBumper()
+        .onTrue(superStructure.runOnce(() -> superStructure.setIntakeMode(SuperStructure.IntakeMode.OFF)));
 
     //driverController.rightBumper().toggleOnTrue(new IntakeCommand());
 
